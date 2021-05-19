56527feb0483265093b411465c023c11d826ff77
vincent@macbook <vincent@macbook.air>
2021-05-19 21:44:10 +0800

    add dist-run

--- script/dist-run.sh
+++ script/dist-run.sh
@@ -1,13 +1,10 @@
   
 #!/bin/sh
 
-root=${PWD}
-echo root=$root
-
 # docker-compose.yml
-echo "curl -sOL https://../Dockerfile"
-curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/tag/dist/Dockerfile.sh
-echo "curl -sOL https://../docker-compose.yml -o ./docker-compose.yml"
+echo 'curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/tag/dist/Dockerfile'
+curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/tag/dist/Dockerfile
+echo 'curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/tag/dist/docker-compose.yml'
 curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/tag/dist/docker-compose.yml
 
 docker-compose up --build --detach
