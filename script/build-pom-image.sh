21d5a2891a13ea4cc6b418f8cc4484fc116d3b5c
huawei laptop <huawei@laptop.com>
2021-05-24 15:38:21 +0800

    fix build-pom-image issue

--- script/build-pom-image.sh
+++ script/build-pom-image.sh
@@ -17,12 +17,12 @@ echo 'curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/tag/image/pom/mvn
 curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/tag/image/pom/mvn.sh
 fi
 
-if [ ! -f ./docker-build.sh ];then
+if [ ! -f ./build-image.sh ];then
 echo 'curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/script/build-image.sh'
 curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/script/build-image.sh
 fi
 
-echo "DOCKER_BUILDKIT=1 docker build . -t $@"
-DOCKER_BUILDKIT=1 docker build . -t $@
+echo "DOCKER_BUILDKIT=1 docker build --no-cache . -t $@"
+DOCKER_BUILDKIT=1 docker build --no-cache . -t $@
 echo ''
 docker images --format "table {{.Repository}}\t{{.Tag}}" $@
