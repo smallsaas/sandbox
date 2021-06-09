c8d247a5da3e26151546c3d9d2851aff667b1bc2
huawei laptop <huawei@laptop.com>
2021-06-09 11:07:35 +0800

    update gateway

--- script/deploy-gateway.sh
+++ script/deploy-gateway.sh
@@ -4,14 +4,17 @@
 # docker-compose.yml
 echo 'curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/tag/deploy/gateway/docker-compose.yml'
 curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/tag/deploy/gateway/docker-compose.yml
+
 if [ ! -d conf.d ];then
   mkdir conf.d
 fi
 
+if [ ! -f ./conf.d/default.conf ];then
 cd conf.d
-echo 'curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/tag/deploy/gateway/conf.d/80.conf'
-curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/tag/deploy/gateway/conf.d/80.conf
+echo 'curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/tag/deploy/gateway/conf.d/default.conf'
+curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/tag/deploy/gateway/conf.d/default.conf
 cd ..
+fi
 
 ## start
 echo "docker-compose up -d $@"
