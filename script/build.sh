7f79cd9a260e0064350d43c336202578ddea03e0
huawei laptop <huawei@laptop.com>
2021-07-16 17:19:34 +0800

    fix some issue

--- script/build.sh
+++ script/build.sh
@@ -1,3 +1,3 @@
-  
 #!/bin/sh
-COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f build.yml up --build --remove-orphans
+COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f build.yml build $@  ## --no-cache
+docker-compose -f build.yml up --remove-orphans
