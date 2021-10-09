04e27c5cf45fb7be808668abc9d39bf3f553f105
vincent@macbook <vincent@macbook.air>
2021-10-09 19:57:19 +0800

    fix build-jar.sh

--- script/build-jar.sh
+++ script/build-jar.sh
@@ -4,13 +4,13 @@
 # docker-compose.yml
 echo 'curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/tag/build/jar/Dockerfile'
 curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/tag/build/jar/Dockerfile
-echo 'curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/tag/build/jar/docker-compose.yml'
-curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/tag/build/jar/docker-compose.yml
+echo 'curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/tag/build/jar/build.yml'
+curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/tag/build/jar/build.yml
 
 if [ ! -f ./build.sh ];then
 echo 'curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/script/build.sh'
 curl -sOL https://gitee.com/smallsaas/sandbox/raw/master/script/build.sh
 fi
 
-echo "COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose up --build $@"
-COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose up --build $@ # --detach
+echo "COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f build.yml up --build $@"
+COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose  -f build.yml up --build $@ # --detach
