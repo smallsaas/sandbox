543b0d745fade6fd0885dc2e44af4c0a6b33e064
Vincent <a30f49@gmail.com>
2021-11-19 09:14:49 +0800

    Create config.js

---
+++ tag/deploy/web/config.js
@@ -0,0 +1,7 @@
+if (window.ZEle === undefined) {
+  window.ZEle = {};
+}
+window.ZEle.endpoint = "";
+window.ZEle.nav = "left"; // LeftNavCollaps left top both
+window.ZEle.indexPage = "";
+window.ZEle.breadcrumb = true;
