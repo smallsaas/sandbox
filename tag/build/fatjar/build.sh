b3917e6b687e75e186ef4db434ff02b619ff4516
cdn@10086.com <cdn@10086.com>
2021-12-19 00:25:44 +0800

    cont.

--- tag/build/fatjar/build.sh
+++ tag/build/fatjar/build.sh
@@ -3,7 +3,7 @@ image='build-fatjar-1'
 ####
 
 if [ ! -f Dockerfile ];then
-docker run --rm -v ${PWD}:/var/build zelejs/sandbox cp /sandbox/tag/build/jar/Dockerfile /var/build/
+docker run --rm -v ${PWD}:/var/build zelejs/sandbox cp /sandbox/tag/build/fatjar/Dockerfile /var/build/
 fi
 
 ## build jar
