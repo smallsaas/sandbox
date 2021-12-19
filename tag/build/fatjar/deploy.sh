560867457c039c1a369a98a508bda3508c9aaf14
cdn@10086.com <cdn@10086.com>
2021-12-19 14:16:04 +0800

    add deploy.sh for build/featjar

---
+++ tag/build/fatjar/deploy.sh
@@ -0,0 +1,135 @@
+#!/usr/bin/env bash
+######################
+deploy_container=${CONTAINER}
+deploy_standalone=${STANDALONE}
+####################
+if [ ! $deploy_container ];then 
+   echo env CONTAINER not yet exported !
+   exit
+fi
+if [ ! $deploy_standalone ];then 
+   echo env STANDALONE not yet exported !
+   exit
+fi
+#########################
+
+ENDPOINT="http://localhost"
+SOCK_OPT='--unix-socket /var/run/docker.sock'
+
+# workingdir(){
+#    endpoint=$1
+#    container=$2
+
+#    # echo "curl -s $endpoint/containers/$container/json | jq '.HostConfig.Binds[] | match(\"([a-z/]+):[a-z/]*/webapps[a-z/]*\").string'"
+#    # local binds=$(curl -s $endpoint/containers/$container/json | jq '.HostConfig.Binds[] | match("([a-z/]+):[a-z/]*/webapps[a-z/]*").string')
+#    echo curl $SOCK_OPT -s $endpoint/containers/$container/json > /dev/stderr
+#    local binds=$(curl $SOCK_OPT -s $endpoint/containers/$container/json | jq '.HostConfig.Binds[] | match("[a-z/]*/webapps:rw").string')
+#    for bind in $binds;do
+#       bind=${bind%\"}
+#       bind=${bind#\"}
+#       bind=${bind%:rw}
+#       echo $bind
+#    done
+# }
+
+getcontainerjsonvalue(){
+   endpoint=$1
+   container=$2
+   jq_filter="$3"
+
+   # echo "curl -s $endpoint/containers/$container/json | jq '.HostConfig.Binds[] | match(\"([a-z/]+):[a-z/]*/webapps[a-z/]*\").string'"
+   # local binds=$(curl -s $endpoint/containers/$container/json | jq '.HostConfig.Binds[] | match("([a-z/]+):[a-z/]*/webapps[a-z/]*").string')
+   echo "curl $SOCK_OPT -s $endpoint/containers/$container/json | jq $jq_filter" > /dev/stderr
+   local result=$(curl $SOCK_OPT -s $endpoint/containers/$container/json | jq $jq_filter)
+   result=${result%\"}
+   result=${result#\"}
+   echo $result
+}
+
+getcontainerworkingdir(){
+   endpoint=$1
+   container=$2
+   echo $(getcontainerjsonvalue $endpoint $container '.Config.Labels."com.docker.compose.project.working_dir"')
+}
+
+getcontainerstatus(){
+   endpoint=$1
+   container=$2
+   echo $(getcontainerjsonvalue $endpoint $container '.State.Status')
+}
+
+stopcontainer(){
+    endpoint=$1
+    container=$2
+    echo curl $SOCK_OPT -s -X POST $endpoint/containers/$container/stop > /dev/stderr
+    curl $SOCK_OPT -s -X POST $endpoint/containers/$container/stop
+}
+restartcontainer(){
+    endpoint=$1
+    container=$2
+    echo curl $SOCK_OPT -s -X POST $endpoint/containers/$container/restart > /dev/stderr
+    curl $SOCK_OPT -s -X POST $endpoint/containers/$container/restart
+}
+
+# buildtar(){
+#    standalone=$1
+#    local dir=${standalone%\/*}
+#    local app=${standalone##*\/}
+#    if [[ $dir = $app ]];then
+#       dir='.'
+#    fi
+
+#    wdir=${PWD}
+#    cd $dir
+#    if [ ! -f $app.tar.gz ];then
+#       echo tar zcvf $app.tar.gz $app > /dev/stderr
+#       tar zcvf $app.tar.gz $app
+#    fi
+#    ls $app.tar.gz
+#    cd $wdir
+# }
+
+putstandalone(){
+    endpoint=$1
+    container=$2
+    working_dir=$3
+    standalone=$4
+
+    echo curl -X PUT ${endpoint}/containers/${container}/archive?path=$working_dir -H \'Content-Type: application/x-tar\' --data-binary @$standalone.tar.gz > /dev/stderr
+    curl -X PUT ${endpoint}/containers/${container}/archive?path=$working_dir -H 'Content-Type: application/x-tar' --data-binary @$standalone.tar.gz
+}
+
+
+### start to deploy
+
+## stop container first
+echo stopping container $deploy_container ..
+stopcontainer $ENDPOINT $deploy_container
+status=$(getcontainerstatus $ENDPOINT $deploy_container)
+if [[ ! $status = 'exited'  ]];then
+   echo "fatal: fail to stop $deploy_container: $status !"
+   exit
+fi
+echo stopped: $status !
+
+
+## build tar
+# echo build tar $deploy_standalone.tar.gz ..
+# buildtar $deploy_standalone
+ 
+## put standalone 
+echo deploying $deploy_standalone ..
+working_dir=$(getcontainerworkingdir $ENDPOINT $deploy_container 2> /dev/null)
+# putstandalone $ENDPOINT $deploy_container $working_dir $deploy_standalone
+app=${deploy_standalone#*/}
+ls -l $working_dir/$app
+echo cp $deploy_standalone $working_dir
+cp $deploy_standalone $working_dir
+ls -l $working_dir/$app
+
+## restart container
+echo restarting container $deploy_container ..
+restartcontainer $ENDPOINT $deploy_container
+
+status=$(getcontainerstatus $ENDPOINT $deploy_container)
+echo restarted: $status ..
